c0d8e5768247530061a95fd1566ad875de068859
Lucas Matheus <lucas0matheus0@gmail.com>
2025-04-18 13:39:02 -0300

    style: estilizando o header e configurando a paleta de cores do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,7 +33,7 @@ export const routes: Routes = [
             },
             {
                 path: 'create',
-                loadChildren: () => CreateRecipeComponent
+                loadComponent: () => CreateRecipeComponent
             },
             {
                 path: 'update',
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatSidenavModule,MatIconModule,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
