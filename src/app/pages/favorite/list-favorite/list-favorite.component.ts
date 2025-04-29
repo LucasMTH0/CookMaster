ba87e5122d089c82be35dacd1093c337c5f34eed
Lucas Matheus <lucas0matheus0@gmail.com>
2025-04-29 19:48:55 -0300

    feat: adicionando o icone de favoritos e o botão de busca de receitas

---
+++ src/app/pages/favorite/list-favorite/list-favorite.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-favorite',
+  imports: [],
+  templateUrl: './list-favorite.component.html',
+  styleUrl: './list-favorite.component.scss'
+})
+export class ListFavoriteComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,14 +2,29 @@ import { Component, inject } from '@angular/core';
 import { RecipeService } from '../../services/recipe/recipe.service';
 import { CardComponent } from '../../components/card/card.component';
 import { AsyncPipe } from '@angular/common';
+import { UserSignal } from '../../signals/user/user';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [CardComponent, AsyncPipe],
+  imports: [CardComponent, AsyncPipe, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   protected recipeService = inject(RecipeService)
+  protected userSignal = inject(UserSignal)
+  protected user = this.userSignal.get()
+  isSearchInputOpen: boolean = false
   recipes$ = this.recipeService.list()
+
+  handleToggleSearchInput(){
+    this.isSearchInputOpen = !this.isSearchInputOpen
+  }
+
+  handleSearchRecipe(event: any){
+    console.log("pesquisou por ", event.target?.value)
+  }
+
+  
 }
